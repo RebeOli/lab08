f177942637d74c3c0fe59f43f13758ab1376c1ae
Rebecca Olivieri <rebecca.olivieri2@studio.unibo.it>
2025-11-12 11:11:06 +0100

    Ex 82: part 1 and 2 finished

--- 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
+++ 82-tdd-deathnote/src/test/java/it/unibo/deathnote/TestDeathNote.java
@@ -1,5 +1,38 @@
 package it.unibo.deathnote;
 
-class TestDeathNote {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertThrowsExactly;
 
+import java.util.concurrent.ExecutionException;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
+
+import it.unibo.deathnote.impl.DeathNoteImpl;
+import it.unibo.deathnote.api.DeathNote;
+
+class TestDeathNote {
+    private static final int RULE=5; 
+    private DeathNote deadNote;
+
+    @BeforeEach
+    void setUp() {
+        deadNote= new DeathNoteImpl();
+    }
+    @Test
+    public void testRule() {
+        int[] array=new int[]{0, -RULE};
+        for (int x : array){
+            try{
+                deadNote.getRule(x);
+            }catch(Exception e){
+                assertEquals(new IllegalArgumentException(), e);
+            }
+        }  
+    }
+
+
+    
 }
